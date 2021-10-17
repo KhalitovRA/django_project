9bba2850a36814146de91d500602cc7c72efccc8
KhalitovRA <halitov-02@mail.ru>
2021-10-17 17:29:16 +0300

    commit migrations, ORM and other

--- films_site/films/models.py
+++ films_site/films/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Films(models.Model):
+    en_title = models.CharField(max_length=150)
+    ru_title = models.CharField(max_length=150)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    duration_film = models.SmallIntegerField()
+    film_score = models.FloatField()
+    photo = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
+    trailer_link = models.CharField(max_length=200)
+    is_active = models.BooleanField(default=True)
--- films_site/films_site/urls.py
+++ films_site/films_site/urls.py
@@ -13,14 +13,20 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
 from films.views import pageNotFound
+from films_site import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('films.urls')),
 ]
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 handler404 = pageNotFound
