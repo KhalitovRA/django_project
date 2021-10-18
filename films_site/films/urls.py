aea6ac9d6018097ed9699061a878b3998ed90a85
KhalitovRA <halitov-02@mail.ru>
2021-10-18 18:55:50 +0300

    commit url in templates

--- films_site/films/urls.py
+++ films_site/films/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from .views import *
 
 urlpatterns =[
+    path('', home, name='home'),
     path('main/', index),
     path('film/<int:pk>/', ViewFilms.as_view()),
 ]
--- films_site/films/views.py
+++ films_site/films/views.py
@@ -5,6 +5,10 @@ from django.views.generic import DetailView
 from films.models import Films
 
 
+def home(request):
+    return render(request, 'films/home_films_list.html')
+
+
 def index(request):
     return render(request, 'films/index.html')
 
