8ec9bfd90ababd2f042de327f1c69bb483299d85
KhalitovRA <halitov-02@mail.ru>
2021-10-31 15:44:58 +0300

    days and weekdays done

--- films_site/films/get_data_info.py
+++ films_site/films/get_data_info.py
@@ -3,14 +3,11 @@ from datetime import datetime
 
 def get_four_days():
     first_day = datetime.today().day
-    four_days = [first_day, first_day+1, first_day+2, first_day+3]
+    four_days = [first_day, (first_day+1) % 31, (first_day+2) % 31, (first_day+3) % 31]
     weekdays = {0: 'пн', 1: 'вт', 2: 'ср', 3: 'чт', 4: 'пт', 5: 'сб', 6: 'вс'}
     w_day = datetime.today().weekday()
-    four_weekdays = [weekdays[w_day], weekdays[w_day+1], weekdays[w_day+2], weekdays[w_day+3]]
-    info = [four_days, four_weekdays]
+    four_weekdays = [weekdays[w_day], weekdays[(w_day+1) % 7], weekdays[(w_day+2) % 7], weekdays[(w_day+3) % 7]]
+    info = {}
+    for item in range(4):
+        info[four_days[item]] = four_weekdays[item]
     return info
-# TODO: если не работает, то возможно все дело в днях неделях
-
-# def get_weekdays():
-
-    # return four_weekdays
